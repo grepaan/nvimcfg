20bb08f5b1b7725fe49b029e8dfe5e2af1869105
Grzegorz Pandyra <gpandyra@cisco.com>
2025-09-23 08:56:42 +0200

    Fix for neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -5,5 +5,8 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
-	}
+	},
+	vim.keymap.set('n', '<leader>n', function ()
+		vim.cmd('Neotree filesystem reveal left toggle')
+	end, {noremap = true, silent = true})
 }
